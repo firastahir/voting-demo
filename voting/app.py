0124aaa2ce619edb1d9eaeb1d7867a9f390139a6
firastahir <firas.tahir@gmail.com>
2018-11-24 23:35:28 +0000

    First push

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Footbal"
+optionB = "Soccer"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
